a5c15389a3706380aed521521ab19891970fa994
RohanMahamuni445 <161917093+RohanMahamuni445@users.noreply.github.com>
2024-12-22 22:44:26 +0530

    Update set-permissions.sh

--- set-permissions.sh
+++ set-permissions.sh
@@ -2,7 +2,7 @@
 # set-permissions.sh
 
 # Ensure that myapp has executable permissions
-chmod +x /var/www/myapp
+sudo chmod +x /var/www/myapp
 
 # Optionally, check if permissions were set correctly
 #ls -l /var/www/myapp
