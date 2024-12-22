fb983adbf8064576ffd65637038e9ddd444e0ac7
rohan445 <rohanmahamuni3315@gmail.com>
2024-12-22 14:15:25 +0530

    file corrected closing paranthesis and if

--- create-web-dir.sh
+++ create-web-dir.sh
@@ -1,15 +1,14 @@
-#!bin/bash
+#!/bin/bash
 
 web_root="/var/www"
 
-# check if /var/www direcotry exists
-
-if [! -d "$web_root" ]; then
-	# if not exists then create it
-	sudo mkdir -p "$web_root"
-	sudo chmod -R ubuntu:ubuntu "$web_root" 
-
+# check if /var/www directory exists
+if [ ! -d "$web_root" ]; then
+    # if not exists then create it
+    sudo mkdir -p "$web_root"
+    sudo chmod -R ubuntu:ubuntu "$web_root"
 fi
 
 rm -f /var/www/*
-echo "the /var/www directory is ready
+echo "the /var/www directory is ready"
+
