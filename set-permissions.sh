971b88c9023b8ff318f7c783ed41c1d653098357
RohanMahamuni445 <161917093+RohanMahamuni445@users.noreply.github.com>
2024-12-22 23:48:33 +0530

    Update set-permissions.sh

--- set-permissions.sh
+++ set-permissions.sh
@@ -3,6 +3,7 @@
 
 # Ensure that myapp has executable permissions
 sudo chmod +x /var/www/myapp
+sudo chmod 755 /var/www/move-html-files.sh
 
 # Optionally, check if permissions were set correctly
 #ls -l /var/www/myapp
