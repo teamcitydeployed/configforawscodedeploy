3191ba03da15e5828711b9853d60a5d8ad701afc
RohanMahamuni445 <161917093+RohanMahamuni445@users.noreply.github.com>
2024-12-22 22:33:59 +0530

    Create set-permissions.sh

---
+++ set-permissions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# set-permissions.sh
+
+# Ensure that myapp has executable permissions
+chmod +x /var/www/myapp
+
+# Optionally, check if permissions were set correctly
+#ls -l /var/www/myapp
